322c76768fae183d72c75b755d06d9021fe59ffd
Wilson Chuei <wilson.chuei@verrency.com>
2024-04-05 20:48:36 +1100

    update test helper

--- test/ring_lambda/api_gw_test_helper.clj
+++ test/ring_lambda/api_gw_test_helper.clj
@@ -3,32 +3,37 @@
             [clojure.java.io :as io]
             [ring.util.codec :as codec]))
 
-(defn gen-event [{:keys [path-prefix path-params proxy-key method query-params] :as opts}]
+(defn gen-event [{:keys [path-prefix path-params proxy-key method query-string body] :as opts}]
   (let [resource (format "%s/{%s+}" path-prefix proxy-key)
         path (format "%s/%s" path-prefix path-params)
-        sample (json/parse-string (slurp (io/resource "sample-apigw-event.json")) true)]
-    (-> sample
-        (assoc :resource resource
-               :path path
-               :method method
-               :pathParameters {proxy-key path-params}
-               :queryStringParameters query-params
-               :multiValueQueryStringParameters (update-vals query-params
-                                                             (fn [v]
-                                                               (if (vector? v)
-                                                                 v
-                                                                 [v]))))
-        (update :requestContext (fn [rc]
-                                  (assoc rc
-                                         :resourcePath resource
-                                         :path resource
-                                         :httpMethod method))))))
+        query-params (codec/form-decode query-string "UTF-8")
+        sample (-> (io/resource "sample-apigw-event.json")
+                   (slurp)
+                   (json/parse-string true)
+                   (assoc :resource resource
+                          :path path
+                          :method method
+                          :pathParameters {proxy-key path-params}
+                          :queryStringParameters query-params
+                          :multiValueQueryStringParameters (update-vals query-params
+                                                                        (fn [v]
+                                                                          (if (vector? v)
+                                                                            v
+                                                                            [v]))))
+                   (update :requestContext (fn [rc]
+                                             (assoc rc
+                                                    :resourcePath resource
+                                                    :path resource
+                                                    :httpMethod method))))]
+    (if body
+      (assoc sample :body body)
+      (dissoc sample :body))))
 
 (comment
   (-> {:path-prefix "/foo"
        :path-params "math/plus"
        :proxy-key "proxy"
        :method "GET"
-       :query-params (codec/form-decode "x=2&y=4" "UTF-8")}
+       :query-string "x=2&y=4"}
       (gen-event)
-      (json/generate-string)))
+      #_(json/generate-string)))
